bf2d229d5f2e9e69409f0186f422cd90a23a859c
Tiffany <tiffquinlan@gmail.com>
2019-12-09 19:50:07 -0700

    Update Burger.java
    updated toppings

--- src/TeamProject/Burger.java
+++ src/TeamProject/Burger.java
@@ -3,7 +3,7 @@ package TeamProject;
 public class Burger extends Food {
 
     private String topping = "n";
-    //validToppings [Pepperoni, sausage, peppers, chicken, salami, tomatoes, olives, anchovies ]; create an array with the valid inputs to check for errors
+    //validToppings [Patty, bacon, cheese, lettuce, tomato, pickles, onions ]; create an array with the valid inputs to check for errors
     //create an additional set and get method for size
 
     @Override // override the Food toString to include the size and toppings in the output

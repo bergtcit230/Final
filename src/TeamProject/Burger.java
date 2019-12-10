39c1cc3a746b910180ba3c8fbd65227789a2b66a
Tiffany <tiffquinlan@gmail.com>
2019-12-09 19:51:50 -0700

    Update Burger.java
    added note

--- src/TeamProject/Burger.java
+++ src/TeamProject/Burger.java
@@ -1,7 +1,7 @@
 package TeamProject;
 
 public class Burger extends Food {
-
+    // inhearets form Food class
     private String topping = "n";
     //validToppings [Patty, bacon, cheese, lettuce, tomato, pickles, onions ]; create an array with the valid inputs to check for errors
     //create an additional set and get method for size
